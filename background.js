99ecd03d61f6e8dc779c763dc42d12a904f5440d
Mike Heavers <mheavers@gmail.com>
2021-02-16 16:34:53 -0800

    should walk through series of search terms - currently not showing a result count. Maybe getting overloaded with async requests?

--- background.js
+++ background.js
@@ -1,43 +1,48 @@
 let clickedTab;
 
-async function find(query) {
+async function find(query, catID) {
   browser.runtime.sendMessage({ msg: "clear-results" });
 
   // If you don't exclude the current tab, every search will find a hit on the
   // current page.
   let this_tab_url = browser.runtime.getURL("find.html");
   let tabs = await browser.tabs.query({});
-  let curTab = await browser.tabs.getCurrent();
-  browser.runtime.sendMessage({
-    msg: "debug",
-    content: curTab,
-    clickedTab,
-  });
-  let askContainer = document.getElementById("ask-btf-container");
 
   for (let tab of tabs) {
     // Iterate through the tabs, but exclude the current tab.
     if (tab.url === this_tab_url) {
       continue;
     }
+    // browser.runtime.sendMessage({
+    //   msg: "debug",
+    //   clickedTab,
+    //   query,
+    //   tab: tab.id,
+    // });
 
     // Call the find API on each tab. We'll wait for the results for each
     // tab before progressing onto the next one by using await.
     //
     // After getting the results, send a message back to the query page
     // and highlight the tab if any results are found.
-    let result = await browser.find.find(query, { tabId: tab.id });
-    if (tab.id == clickedTab) {
+
+    let result = await browser.find.find(query, {
+      tabId: tab.id,
+      // includeRangeData: true,
+      // includeRectData: true,
+    });
+
+    if (tab.id == clickedTab && result.count) {
       browser.runtime.sendMessage({
         msg: "found-result",
         id: tab.id,
         url: tab.url,
         count: result.count,
+        catID: catID,
       });
-    }
-
-    if (result.count) {
-      browser.find.highlightResults({ tabId: tab.id });
+      browser.find.highlightResults({
+        tabId: tab.id,
+      });
     }
   }
 }
--- find.js
+++ find.js
@@ -1,8 +1,166 @@
 let backgroundPage = browser.extension.getBackgroundPage();
 
+const VALUES_DATA = {
+  environment: [
+    "sustainability",
+    "biodiversity",
+    "pollutant",
+    "organic",
+    "pollution",
+    "organism",
+    "biology",
+    "biological",
+    "ecosystem",
+    "microbe",
+    "microorganism",
+    "contaminant",
+    "ecology",
+    "environmentalist",
+    "pesticide",
+    "humidity",
+    "forest",
+    "deforestation",
+    "outdoor",
+    "conserve",
+    "conservation",
+    "sediment",
+    "green",
+    "contamination",
+    "weather",
+    "urbanization",
+    "waste",
+    "epa",
+    "compost",
+    "eco",
+    "wastewater",
+    "warming",
+    "natural",
+    "biological",
+    "sensing",
+    "species",
+    "monitoring",
+    "carbon",
+    "degradation",
+    "recycle",
+    "chemical",
+    "resource",
+  ],
+  small_business: [
+    "small business",
+    "independent",
+    "women owned",
+    "black owned",
+    "bipoc",
+    "craft",
+    "hand made",
+    "non-profit",
+    "african american",
+    "equality",
+    "equity",
+    "ethnic",
+    "multicultural",
+    "local",
+    "one of a kind",
+    "female owned",
+    "artisan",
+    "non profit",
+    "alliance",
+    "unique",
+    "sustainable",
+    "regional",
+    "handmade",
+    "native",
+    "feminist",
+    "equitable",
+    "fair trade",
+    "inclusive",
+    "diverse",
+    "accessible",
+    "equal",
+    "opportunity",
+    "outreach",
+    "public",
+    "nonprofit",
+    "good",
+    "benefit",
+    "support",
+    "neighborhood",
+    "area",
+    "market",
+    "brick and mortar",
+    "shop small",
+    "alliance",
+    "marketplace",
+    "union",
+  ],
+  privacy: [
+    "privacy",
+    "encryption",
+    "secure",
+    "security",
+    "encrypted",
+    "anonymous",
+    "robust",
+    "authentication",
+    "vault",
+    "protect",
+    "authenticate",
+    "private",
+    "antivirus",
+    "standards",
+    "data",
+    "password",
+    "safeguard",
+    "vulnerable",
+    "biometric",
+    "scanning",
+    "breech",
+    "warranty",
+    "permission",
+    "location",
+    "hacker",
+    "disclosure",
+    "guarantee",
+    "algorithm",
+    "artificial intelligence",
+    "blockchain",
+    "connection",
+    "crypto",
+    "framework",
+    "decentralize",
+    "cybersecurity",
+    "authority",
+    "trusted",
+    "certificate",
+    "credential",
+    "trust",
+    "personal",
+  ],
+};
+
+let valueWeights = {
+  environment: { weight: 50, count: 0 },
+  small_business: {
+    weight: 50,
+    count: 0,
+  },
+  privacy: { weight: 50, count: 0 },
+};
+
+let resultsFound = false; //true when we have at least one match
+
 document.getElementById("find-form").addEventListener("submit", function (e) {
   // Send the query from the form to the background page.
-  backgroundPage.find(document.getElementById("find-input").value);
+  document.querySelectorAll(".form-control-range").forEach((rangeSlider, i) => {
+    const thisID = rangeSlider.dataset.id;
+    valueWeights[thisID].weight = rangeSlider.value;
+    for (let i = 0; i < VALUES_DATA[thisID].length; i++) {
+      const thisTerm = VALUES_DATA[thisID][i];
+      backgroundPage.find(thisTerm, thisID);
+    }
+  });
+  //backgroundPage.find(document.getElementById("find-input").value);
+
   e.preventDefault();
 });
 
@@ -20,13 +178,23 @@ function handleMessage(request, sender, response) {
     results.innerHTML = "";
   }
   if (request.msg === "found-result") {
-    console.log(request);
+    //console.log(request);
+    if (resultsFound === false) {
+      resultsFound = true;
+      resultsContainer.classList.toggle("active", true);
+    }
+    const thisVal = valueWeights[request.catID];
     // List out responses from the background page as they come in.
-    let li = document.createElement("li");
-    li.innerHTML = `${request.count} result(s) found on <a href="${request.url}">this page</a> `;
-    results.appendChild(li);
-    resultsContainer.classList.toggle("active", true);
+    thisVal.count += request.count;
+    //let li = document.createElement("li");
+    //li.innerHTML = `${request.count} result(s) found on <a href="${request.url}">this page</a> `;
+    //results.appendChild(li);
+    const curScore = thisVal.count * (thisVal.weight / 100);
+    document.querySelector(
+      `.score-item[data-id="${request.catID}"] .score`
+    ).innerText = `${curScore}`;
   }
+
   if (request.msg === "debug") {
     console.log(request);
   }
